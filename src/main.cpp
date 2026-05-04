308cda00125cb6083de16dbccc6a9ac7aa812ffd
ardaelam <artem.elesin2007@gmail.com>
2026-05-04 20:19:57 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 // Copyright 2022 NNTU-CS
 #include "tpqueue.h"
 
+struct SYM {
+    char ch;
+    int prior;
+    SYM(char c, int p) : ch(c), prior(p) {}
+};
+
 int main() {
     TPQueue<SYM> pqueue;
-    pqueue.push(SYM{'a', 4});
-    pqueue.push(SYM{'b', 7});
+    pqueue.push(SYM('a', 4));
+    pqueue.push(SYM('b', 7));
     SYM c1 = pqueue.pop();
     SYM c2 = pqueue.pop();
     return 0;
